56c17fb3031b4ef86127b366feb8fa127d539031
Michael Bombini <bombini.michael@gmail.com>
2022-06-21 17:01:06 +0200

    Push test

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,14 +1,49 @@
 <template>
     <footer>
         <div class="container">
-
+            <div class="row">
+                <div class="col-4" v-for="link in footerLink" :key="link.nomeLink">
+        
+                
+                    <h2>{{link.nomeLink}}</h2>
+                    <ul>
+                        <li v-for="(linkTest,i) in footerLink.link" :key="i">A</li>
+                    </ul>
+            
+             
+                </div>
+                
+            </div>
         </div>
     </footer>
 </template>
 
 <script>
     export default {
-        
+        name : "TheFooter",
+        data(){
+            return{
+                footerLink : [
+                {
+                 nomeLink : "DC COMICS",
+                 link : ["Character" , "Comics" , "Movies" , "TV" , "Games" , "Videos" , "News"],
+                },
+                {
+                 nomeLink : "SHOP",
+                 link :  ["Shop DC" , "Shop DC Collectibles"],
+                },
+                {
+                 nomeLink : "DC",
+                 link : ["Terms Of Use" , "Privacy Policy (New)" , "Ad Choices" , "Advertising" , "Jobs" , "Subscriptions" , 
+                        "Talent Workshops" , "CPSC Certificates" , "Ratings" , "Shop Help" , "Contact Us"],
+                },
+                {
+                 nomeLink : "SITES",
+                 link : ["DC" , "MAD Magazine" , "DC Kids" , "DC Universe" , "DC Power Visa"],
+                },
+            ]
+            }
+        }
     }
 </script>
 
@@ -19,8 +54,13 @@
 
 footer {
     background-image: url("../assets/img/footer-bg.jpg");
-    padding: 200px 0;
+    padding-top: 80px;
+    padding-bottom: 100px;
     background-repeat: no-repeat;
     background-size: cover;
+    color: #fff;
 }
+
+
+
 </style>
