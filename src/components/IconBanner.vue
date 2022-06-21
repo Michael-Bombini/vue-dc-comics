49a55042b73dc8d97e2c85493262cf1640af5d13
Michael Bombini <bombini.michael@gmail.com>
2022-06-21 16:20:57 +0200

    Completed IconBanner component

--- src/components/IconBanner.vue
+++ src/components/IconBanner.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="banner p-5">
         <div class="container">
-            <ul>
+            <ul class="d-flex justify-content-around">
                 <li v-for="icon in icons" :key="icon">
-                    <img :src="`../assets/img/buy-${icon.iconName}.${icon.extension}`" alt="">
+                        <img :src="require(`@/assets/img/buy-${icon.iconName}.${icon.extension}`)" alt="icon.iconName">
+                        <span>{{icon.title}}</span>
+
                 </li>
             </ul>
         </div>
@@ -21,22 +23,27 @@
                     {
                     iconName : "comics-digital-comics",
                     extension : "png",
+                    title : "DIGITAL COMICS",
                     },
                     {
                     iconName : "comics-merchandise",
                     extension : "png",
+                    title : "DC MERCHANDDISE",
                     },
                     {
                     iconName : "comics-shop-locator",
                     extension : "png",
+                    title : "SUBSCRIPTION",
                     },
                     {
                     iconName : "comics-subscriptions",
                     extension : "png",
+                    title : "COMIC SHOP LOCATOR",
                     },
                     {
                     iconName : "dc-power-visa",
                     extension : "svg",
+                    title : "DC POWER VISA",
                     }
                     
                     
@@ -55,6 +62,18 @@
 
 .banner {
     background-color: $light-blue;
+
+img {
+    width:  60px;
+    height: 60px;
+    object-fit: contain;
+} 
+
+span {
+    color: $white;
+    padding: 0.3rem;
+    font-size: 14px;
+}
 }
 
 </style>
