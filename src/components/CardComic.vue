701db980f2632a4ae2b185d0ce07551478219ea6
Michael Bombini <bombini.michael@gmail.com>
2022-06-22 13:47:47 +0200

    First test added images comics

---
+++ src/components/CardComic.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" :src="imgUrl" :alt="`Fumetto della serie di ${series}`"/>
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+     name : "CardComic",
+     props: {
+        imgUrl : String,
+        price : String,
+        series : String,
+        type : String,
+     }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
